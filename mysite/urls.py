e75272abb6b62bc4800482f915217815c26378dd
unknown <jhanvijoshi12@gmail.com>
2022-11-06 12:40:01 +0530

    changed procfile.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -40,4 +42,4 @@ urlpatterns = [
 urlpatterns += [
     path('admin/', admin.site.urls),
     path('api/v1/', include('star_studio.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
